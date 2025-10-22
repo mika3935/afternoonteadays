21c872dcdb8dbf8a037827276af8f8086b86baac
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-10-22 22:04:52 +0900

    コンパイル８

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,7 @@
 # Be sure to restart your server when you modify this file.
-
-# バージョン管理（キャッシュ対策）
 Rails.application.config.assets.version = "1.0"
 
-# 画像ディレクトリを追加
+# 画像パスを追加
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
 
-# 追加でプリコンパイルしたいJSがある場合
-# （今回はSprockets経由でJSは不要なのでコメントアウト）
-# Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
+# JSはImportmapで管理するのでプリコンパイル設定不要
