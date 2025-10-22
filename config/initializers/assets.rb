991f52cafc204b9bccd2c39d53e4ca75dbec2b94
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-10-22 22:26:59 +0900

    プリコンパイル１０

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,5 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
 
 # JSはImportmapで管理するのでプリコンパイル設定不要
+# Rails.application.config.assets.precompile += %w( application.js )
+# Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
