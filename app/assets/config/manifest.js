8acbf05579db4c085f8637c2d6134a2bc3f22fe9
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-10-22 21:52:23 +0900

    プリコンパイル７

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,2 @@
-// app/assets/config/manifest.js
-
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,16 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-# アセットのバージョン
+# バージョン管理（キャッシュ対策）
 Rails.application.config.assets.version = "1.0"
 
 # 画像ディレクトリを追加
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
 
-# CSSだけプリコンパイルする場合（JSはなし）
-# デフォルトで app/assets/stylesheets/*.css はプリコンパイルされるので基本不要
-# 追加でプリコンパイルしたいCSSがあれば下記のように追記可能
-# Rails.application.config.assets.precompile += %w( custom.css )
-
-# JS関連のプリコンパイル設定は削除
+# 追加でプリコンパイルしたいJSがある場合
+# （今回はSprockets経由でJSは不要なのでコメントアウト）
 # Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
-# Rails.application.config.assets.precompile += %w( application.js )
