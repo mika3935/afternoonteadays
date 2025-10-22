ac28e44f2ad1e6d0043fdc3e844f17c2e17978f5
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-10-22 21:08:41 +0900

    プリコンパイル６

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
+# アセットのバージョン
 Rails.application.config.assets.version = "1.0"
 
 # 画像ディレクトリを追加
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
 
-# JSのプリコンパイル
-Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
+# CSSだけプリコンパイルする場合（JSはなし）
+# デフォルトで app/assets/stylesheets/*.css はプリコンパイルされるので基本不要
+# 追加でプリコンパイルしたいCSSがあれば下記のように追記可能
+# Rails.application.config.assets.precompile += %w( custom.css )
 
-Rails.application.config.assets.precompile += %w( application.js )
+# JS関連のプリコンパイル設定は削除
+# Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
+# Rails.application.config.assets.precompile += %w( application.js )
