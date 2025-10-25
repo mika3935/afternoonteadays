9795f1c3f5fc4fcffb902451496cc439db340fa2
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-10-26 00:39:26 +0900

    プリコンパイル１３

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link application.js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js# Be sure to restart your server when you modify this file.
 
