bf8a4cdca622b4698dfefe13e9a4bc4ebd91f844
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-10-29 23:33:07 +0900

    プリコンパイル２２

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,6 @@
 // app/javascript/application.js
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
-import "popper.js"
 import "jquery"
+import "@popperjs/core"
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# Bootstrap & JS dependencies
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+# Bootstrap関連
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
