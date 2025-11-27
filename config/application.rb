325935cd9d062375c98764f58787336bfc9eb944
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-11-28 00:40:59 +0900

    re re re re recloudinary

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,17 @@
-# config/application.rb
 require_relative "boot"
+
 require "rails/all"
-require "refile/rails"
 
 # Gemfile に記載された gem を読み込む
 Bundler.require(*Rails.groups)
 
 module Afternoonteadays
   class Application < Rails::Application
-    # Rails 7.2 の設定を使う
     config.load_defaults 7.1
-    config.i18n.default_locale = :ja 
+
+    # i18n設定（日本語化）
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:en, :ja]
 
     # lib配下の自動読み込み設定
     config.autoload_lib(ignore: %w(assets tasks))
@@ -18,10 +19,6 @@ module Afternoonteadays
     # タイムゾーン設定
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
-
-    # i18n設定（日本語化）
-    config.i18n.default_locale = :ja
-    config.i18n.available_locales = [:en, :ja]
 
     # ジェネレータ設定（不要ファイルを生成しない）
     config.generators do |g|
