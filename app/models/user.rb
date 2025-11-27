3e6d98a283b5147f13a1f8e98ea71b4a9e57274b
mika3935 <226095606+mika3935@users.noreply.github.com>
2025-11-28 00:47:15 +0900

    re cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,3 +31,35 @@ class User < ApplicationRecord
     following_user.include?(user)
   end  
 end
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+  # ✅ Refile の attachment を削除
+  # attachment :profile_image
+
+  # ✅ Active Storage を使う場合はこちら
+  has_one_attached :profile_image
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  validates :name, presence: true
+
+  def following?(user)
+    following_user.include?(user)
+  end
+end
